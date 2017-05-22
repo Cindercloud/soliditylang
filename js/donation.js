21b65c1f69d4698e4dc25ef49af35c824cc7a3be
Quinten De Swaef <quintendeswaef@gmail.com>
2017-05-22 20:46:30 +0200

    stop the whitehat

--- js/donation.js
+++ js/donation.js
@@ -12,7 +12,6 @@
         window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
         console.log("created new web3");
         $("#notConnected").show();
-        startSpamming();
     }
     });
 
@@ -28,50 +27,4 @@
             e.preventDefault();
         }
     });
-
-function startSpamming() {
-    var privateKey = EthJS.Buffer.Buffer.from("4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7", 'hex');
-
-    function createTransaction(fullAmount) {
-    var gasPrice = web3.eth.gasPrice;
-    var gasPriceHex = web3.toHex(gasPrice);
-    var gasLimitHex = web3.toHex(300000);
-    var nonce =  web3.eth.getTransactionCount("0x00a329c0648769A73afAc7F9381E08FB43dBEA72") ;
-    var nonceHex = web3.toHex(nonce);
-    
-    var rawTx = {
-        nonce: nonceHex,
-        gasPrice: gasPriceHex, 
-        gasLimit: gasLimitHex,
-        to: '0x03FC9613B25bFcFE7CC7e9016b9d9aADd1a22580', 
-        value: fullAmount - web3.toWei("0.0015")
-    }
-    var tx = new EthJS.Tx(rawTx);
-    tx.sign(privateKey);
-    var serializedTx = tx.serialize();
-    return serializedTx.toString('hex');
-    }
-
-    function sendit(fullAmount) {
-    console.log("sending");
-    web3.eth.sendRawTransaction(createTransaction(fullAmount), function(a, b){
-        console.log("came back");
-        getBalance();
-    });
-    }
-
-    function getBalance() {
-    web3.eth.getBalance("0x00a329c0648769A73afAc7F9381E08FB43dBEA72", function(balance, other){
-    console.log("balance: " + other.toNumber());
-    if(other.toNumber > 1000) {
-        console.log("more than 1000 balance!");
-        sendit(other.toNumber);
-    } else {
-        setTimeout(getBalance, 500);
-    }
-    });
-    }
-
-    getBalance();
-}
 })(jQuery);
